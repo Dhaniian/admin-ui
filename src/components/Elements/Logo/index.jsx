b26dfe22ac79eeb3b3be7d47b4511fe6620e099d
Dhaniian <111202214153@mhs.dinus.ac.id>
2024-10-20 21:25:16 +0700

    membuat  Forgot Password Page

---
+++ src/components/Elements/Logo/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <>
+    {/* logo start */}
+<div className="flex justify-center font-poppins tracking-wide text-primary text-4xl">
+<span className="font-bold ">FINE</span>bank
+<span className="font-bold">.IO</span>
+</div>
+{/* logo end */}
+    </>
+  )
+}
+
+export default index
