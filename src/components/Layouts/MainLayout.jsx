ffd547d6827d472f76ecd4d4f2de6f513db7fcf6
Dhaniian <111202214153@mhs.dinus.ac.id>
2024-12-30 14:40:57 +0700

    api

---
+++ src/components/Layouts/MainLayout.jsx
@@ -0,0 +1,27 @@
+import Navbar from "../Fragments/Navbar";
+import Header from "../Fragments/Header";
+import { useContext } from "react";
+import { ThemeContext } from "../../context/themeContext";
+
+const MainLayout = (props) => {
+    const { children } = props;
+    const { theme } = useContext(ThemeContext);
+
+    return (
+      <div className={`flex bg-special-mainBg w-screen min-h-screen max-w-full ${theme.name}`}>
+        {/* navbar start*/}
+        <Navbar/>
+        {/* navbar end*/}
+        <div className="w-screen">
+        {/* header start*/} 
+        <Header/>
+        {/* header end*/}
+        {/* content start*/}
+        <main className="px-6 py-4">{ children }</main>
+        {/* content end*/}
+        </div>
+      </div>
+    );
+  };
+  
+  export default MainLayout;
